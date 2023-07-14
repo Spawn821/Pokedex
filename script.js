f2a2c2911433c53944bb28069410e077505cb175
Stephan Schmidt <stephan.schmidt821@gmail.com>
2023-07-14 20:16:31 +0200

    implement small view layout on list screen and json with rgb colors equal species color

--- script.js
+++ script.js
@@ -1,52 +1,84 @@
-let pokemonList = [];
-let backgroundColorList = [];
+let pokemonSpeciesColors = [
+    {
+        'name': 'black',
+        'rgb': 'rgb(112,87,70)'
+    },
+    {
+        'name': 'blue',
+        'rgb': 'rgb(99,144,240)'
+    },
+    {
+        'name': 'brown',
+        'rgb': 'rgb(168,167,122)'
+    },
+    {
+        'name': 'gray',
+        'rgb': 'rgb(183,183,206)'
+    },
+    {
+        'name': 'green',
+        'rgb': 'rgb(122,199,76)'
+    },
+    {
+        'name': 'pink',
+        'rgb': 'rgb(214,133,173)'
+    },
+    {
+        'name': 'purple',
+        'rgb': 'rgb(115,86,151)'
+    },
+    {
+        'name': 'red',
+        'rgb': 'rgb(238,129,49)'
+    },
+    {
+        'name': 'white',
+        'rgb': 'rgb(150,217,214)'
+    },
+    {
+        'name': 'yellow',
+        'rgb': 'rgb(247,208,43)'
+    }
+];
 
-async function load() {
-    let test = await returnJSON('https://pokeapi.co/api/v2/pokemon-species/150/');
-    let test2 = await returnJSON('https://pokeapi.co/api/v2/pokemon/150/');
-    let test3 = await returnJSON('https://pokeapi.co/api/v2/pokemon-color/3/');
 
-    console.log(test);
-    console.log(test2);
-    console.log(test3);
-}
-
-
-async function loadPokemon() {
-    let pokemon = await returnJSON('https://pokeapi.co/api/v2/pokemon?offset=0&limit=150');
+async function renderPokemonList() {
+    let pokemon = await returnJSON('https://pokeapi.co/api/v2/pokemon/1/');
+    let pokemonSpecies = await returnJSON('https://pokeapi.co/api/v2/pokemon-species/1/');
+    let color = await returnJSON('https://pokeapi.co/api/v2/pokemon-color/');
+    let form = await returnJSON('https://pokeapi.co/api/v2/pokemon-form/1/');
+    let item = await returnJSON('https://pokeapi.co/api/v2/item/4/');
 
     console.log(pokemon);
+    console.log(pokemonSpecies);
+    console.log(color);
+    console.log(form);
+    console.log(item);
 
-    for (let i = 0; pokemon['results'].length; i++) {
-        const currentPokemon = pokemon['results'][i]['name'];
-        let pokemonImg = await returnJSON(pokemon['results'][i]['url']);
-
-        loadBackgroundColor(currentPokemon);
-
-        document.getElementById('pokemonImg').src = pokemonImg['sprites']['other']['official-artwork']['front_default'];
-    }
 }
 
 
-async function loadBackgroundColor(currentPokemon) {
-    let backgroundColor = await returnJSON('https://pokeapi.co/api/v2/pokemon-color');
-
-
-    for (let i = 0; i < backgroundColor['results'].length; i++) {
-        const color = await returnJSON(backgroundColor['results'][i]['url']);
-
-        for (let j = 0; j < color['pokemon_species'].length; j++) {
-            const pokemonSpecies = color['pokemon_species'][j]['name']
-
-            if (pokemonSpecies == currentPokemon) {
-                let pokemonBackgroundColor = await returnJSON(color['pokemon_species'][j]['url']);
-
-                console.log(pokemonBackgroundColor['color']['name']);
-
-                document.getElementById('pokemonCard').style = `background-color: ${pokemonBackgroundColor['color']['name']}`;
-            }
-        }
-    }
+function getHTMLPokemonList() {
+    return /*html*/`
+        <div class="pokemonSmallCard">
+            <div class="pokemonSmallCardText">
+                <span id="pokemonSmallCardHeadline">Bisasam</span>
+                <span id="pokemonSmallCardSubheadline">Pflanze</span>
+            </div>
+            <img id="pokemonSmallCardImg"
+                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png">
+            <img id="pokeballSmallCardImg" src="./img/pokeball.png">
+        </div>
+        <div class="pokemonSmallCard">
+            <div class="pokemonSmallCardText">
+                <span id="pokemonSmallCardHeadline">Bisasam</span>
+                <span id="pokemonSmallCardSubheadline">Pflanze</span>
+            </div>
+            <img id="pokemonSmallCardImg"
+                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png">
+            <img id="pokeballSmallCardImg" src="./img/pokeball.png">
+        </div>
+    `;
 }
 
 
